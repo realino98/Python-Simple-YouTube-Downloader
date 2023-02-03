f6f6274a27c744d564b0fc0997e9e326a331575a
Bernard <bernard.realino98@gmail.com>
2023-02-03 16:04:33 +0700

    BR : Added Feature - Modify UI - Added Video Chekcing Feature

--- Main.py
+++ Main.py
@@ -7,32 +7,41 @@ link="https://www.youtube.com/watch?v=igeBnwymK5o"
 yt = YouTube(link)
 
 layout = [
-    [sg.Text("Insert Link : "), sg.InputText(link)],
-    [sg.ProgressBar(100, orientation='h', s=(10,20), k='-PBAR-')],
+    [sg.Text("YouTube Link : "), sg.InputText(link, k='-link-'), sg.Button("Check")],
+    [sg.Text("Video Available : ", k='-video_title_status-', visible=False), sg.Text("", k="-video_title-", visible=False)],
     [sg.Text("Status : "), sg.Text("", k="-status-")],
-    [sg.Button("Download"), sg.Button("Exit")],
-    [sg.Multiline(s=(15,2), k="-log-"), ]
+    [sg.ProgressBar(100, orientation='h', s=(55,15), k='-progress_bar-')],
+    [sg.Multiline(k="-log-", s=(100, 20))],
+    [sg.Button("Download"), sg.Button("Exit")]
 ]
 
 window = sg.Window('YouTube Downloader', layout)
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    if event == sg.WIN_CLOSED or event == 'Exit':                       
         break
+    if event == "Check":
+        if values["-link-"] != "":
+            yt = YouTube(values["-link-"])
+            window['-video_title_status-'].update(visible=True)
+            window['-video_title-'].update(yt.title, visible=True)
     if event == "Download":
-        if event[0] != "":
-            window['-PBAR-'].update(0)                                                     # Show 30% complete on ProgressBar
+        if values["-link-"] != "":
+            yt = YouTube(values["-link-"])
+            window['-progress_bar-'].update(0)                                                     # Show 30% complete on ProgressBar
             print("Downloading", yt.title)
-            window['-PBAR-'].update(10)                                                     # Show 30% complete on ProgressBar
+            window['-progress_bar-'].update(10)                                                     # Show 30% complete on ProgressBar
             window['-status-'].update("Downloading " + yt.title)
-            yt = YouTube(link)
-            window['-PBAR-'].update(40)                                                     # Show 30% complete on ProgressBar
-            yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(SAVE_PATH + yt.title + ".mp4")
-            window['-PBAR-'].update(70)                                                     # Show 30% complete on ProgressBar
+            window['-progress_bar-'].update(40)                                                     # Show 30% complete on ProgressBar
+            window['-progress_bar-'].update(70)                                                     # Show 30% complete on ProgressBar
+            yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(SAVE_PATH)
             window['-status-'].update("Download Complete!")                                                     # Show 30% complete on ProgressBar
-            window['-PBAR-'].update(100)                                                     # Show 30% complete on ProgressBar
-            print(yt.title, "Downloaded")
+            window['-link-'].update("")                                                     # Show 30% complete on ProgressBar
+            window['-progress_bar-'].update(100)                                                     # Show 30% complete on ProgressBar
             window['-log-'].update(yt.title + " Downloaded Successfully")
+        else:
+            window['-log-'].update("Invalid Link")
+
     
 window.close()
